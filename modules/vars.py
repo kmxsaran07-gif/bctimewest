301bc980492683e208f8d92425248c3ede9adff0
kmxsaran07-gif <kmxsaran07@gmail.com>
2026-02-07 09:51:43 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,16 +5,16 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8048022999:AAF4hvMZO868n31Mlqf1A3whZ0c5Vi7-tko")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "5302639580"))
-CREDIT = environ.get("CREDIT", 'Ankit')
+OWNER = int(environ.get("OWNER", "6966002582"))
+CREDIT = environ.get("CREDIT", 'kmx')
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5302639580').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6966002582').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5302639580').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '6966002582').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -33,6 +33,7 @@ photoyt = 'https://tinypic.host/images/2025/03/18/YouTube-Logo.wine.png' #https:
 photocp = 'https://tinypic.host/images/2025/03/28/IMG_20250328_133126.jpg'
 photozip = 'https://envs.sh/cD_.jpg'
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
